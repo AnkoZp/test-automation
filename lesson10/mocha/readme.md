f0b45ba4ad36034e9a9f149ee2e37149dafd1830
Anastasiia Panchenko <a.a.panchenko25@gmail.com>
2025-10-22 01:43:25 +0200

    HT lesson10

---
+++ lesson10/mocha/readme.md
@@ -0,0 +1,8 @@
+This is the basic project for TS
+Recommended config for programming on TS >  https://coda.io/d/QA-Automation_d3DDrmEyeIe/TS_suhLQUIn#_lutrAdCg
+
+Project Structure
+test with using Mocha
+
+How to Run
+npm run test
---
+++ lesson10/mocha/src/main.ts
@@ -0,0 +1,29 @@
+import { Transport } from './abstraction/i-transport';
+import { Car } from './classes/class-car';
+import { ElectricCar } from './classes/class-electrical-car';
+import { Airplane } from './classes/class-airplane';
+
+function operateTransport(vehicle: Transport): void {
+    console.log(vehicle.getInfo());
+    vehicle.startEngine();
+    vehicle.setSpeed(100);
+    vehicle.gainingInSpeed(50);
+    vehicle.stopEngine();
+    console.log('---------------');
+}
+
+const bmw = new Car('BMW M3', 250);
+const tesla = new ElectricCar('Tesla Model S', 220, 80);
+const boeing = new Airplane('Boeing 737', 900);
+
+operateTransport(bmw);
+operateTransport(tesla);
+
+console.log('Actions with airplanes:');
+boeing.startEngine();
+boeing.setSpeed(300);
+(boeing as Airplane).takeOff();
+boeing.gainingInSpeed(200);
+(boeing as Airplane).land();
+boeing.stopEngine();
+console.log(boeing.getInfo());
