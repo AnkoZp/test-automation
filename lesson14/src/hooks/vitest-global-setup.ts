f36fd78fd01c074fc990b1df6ec4bd7f320f9380
Anastasiia Panchenko <a.a.panchenko25@gmail.com>
2025-11-04 20:24:57 +0100

    delete some files

--- lesson14/src/hooks/vitest-global-setup.ts
+++
@@ -1,9 +0,0 @@
-// import { initGlobal } from '../global';
-import { ApiWorld } from '../api.world';
-
-// exports.mochaGlobalSetup = function () {
-//     console.log('global hook');
-//     initGlobal();
-// };
-const world = new ApiWorld();
-export { world };
--- lesson14/src/models/the-cats-api/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './image.dto';
-export * from './breeds.dto';
-export * from './votes.dto';
-export * from './favourites.dto';
